24d9d20be699e18bc0a6256c27b2ccdf828c61f4
Fumck Somciety <sayantandas30011998@gmail.com>
2021-01-27 16:11:44 +0530

    removed speak, will save.

--- audiobook/__init__.py
+++ audiobook/__init__.py
@@ -5,8 +5,9 @@ import PyPDF2
 
 
 class AudioBook:
-    def __init__(self, book_path):
+    def __init__(self, book_path,spage):
         self.book_path = book_path
+        self.start_page = int(spage)
 
     def text_to_speech(self):
         with open(self.book_path, "rb") as book:
@@ -18,8 +19,7 @@ class AudioBook:
             engine = pyttsx3.init()
             engine.setProperty("rate", 125)
 
-            start_page = int(input("Please enter the page number: "))
-            for num in range(start_page, pages):
+            for num in range(self.start_page, pages):
                 print("Reading page number " + str(num) + " page!")
                 page = pdfReader.getPage(num)
                 text = page.extractText()
